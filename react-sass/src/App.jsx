b4f693e884838198bef33a94440e34e5c4eac4c0
Vinit Patidar <Vinitpatidar780@gmail.com>
2023-12-20 17:48:05 +0530

    "changes"

---
+++ react-sass/src/App.jsx
@@ -0,0 +1,18 @@
+import './styles/app.scss'
+
+function App() {
+
+  return (
+    <>
+      <div className='productCard'>
+          <h2>No 1 Product</h2>
+          <a href="/">Purchase</a>
+          <p>click paragraph</p>
+          <button>click</button>
+      </div>
+
+    </>
+  )
+}
+
+export default App
